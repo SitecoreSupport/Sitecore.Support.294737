225b34173d697bdb4eb581aeb383b574428375c8
DK\dmsa <dmsa@sitecore.net>
2018-11-23 11:02:17 +0200

    Origin code of the "Email.cshtml" file.

---
+++ src/Sitecore.Support.294737/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.294737")]
+[assembly: AssemblyProduct("Sitecore.Support.294737")]
+[assembly: ComVisible(false)]
